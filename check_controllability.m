5a37121ffbf3f649089585414b59a9602e915f7f
nstienen <n.l.stienen@student.tudelft.nl>
2024-03-22 11:35:07 +0100

    Create check_controllability.m

---
+++ check_controllability.m
@@ -0,0 +1,12 @@
+function check_controllability(sysc)
+
+    Ac = sysc.A;
+    Bc = sysc.B;
+
+    Ctrb_rank = rank(ctrb(Ac,Bc));
+    disp('Number of states');
+    disp(size(Ac));
+    disp('Rank of controllability matrix');
+    disp(Ctrb_rank);
+
+end
