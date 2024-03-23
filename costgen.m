72210d93e11f38d30358471b4890bb0618076fb5
Niels <niels.stienen123@gmail.com>
2024-03-23 18:22:25 +0100

    Commented

--- costgen.m
+++ costgen.m
@@ -1,10 +1,18 @@
 function [H,h,c]=costgen(T,S,Q,R,dim,x0,P,M)
 
+% Cost function generation
+% Converts a cost function of the form:
+% V_N(x(0),u_n) = 0.5*sum_{0,N-1}(x'Qx + u'Ru + 2x'Mu) + 0.5x_N'Px_N
+% into a form only dependent on u_N:
+% V_N(u_N) = 0.5u'Hu + h'u + c
+
+% Define the bar matrices Qbar, Rbar, Mbar
 Qbar = kron(eye(dim.N),Q); 
 Qbar(end-dim.nx+1:end,end-dim.nx+1:end) = P;
 Rbar = kron(eye(dim.N),R);
 Mbar = [kron(eye(dim.N),M);zeros(2*dim.N*dim.nu,dim.N*dim.nu)];
 
+% Calculate H,h,c
 H = Rbar + S'*Qbar*S + 2*S'*Mbar;  
 h = S'*Qbar*T*x0 + Mbar'*T*x0;
 c = x0'*T'*Qbar*T*x0;
